c48b70044357af4ac0392934b75dca4d664c55bd
themarkappleby <themarkappleby@gmail.com>
2020-02-07 11:55:32 -0500

    canvas alpha and resize

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
 
 var scene = new THREE.Scene()
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 5
 
-var renderer = new THREE.WebGLRenderer()
+var renderer = new THREE.WebGLRenderer({
+  alpha: true
+})
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 var geometry = new THREE.BoxGeometry()
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+var material = new THREE.MeshBasicMaterial({ color: 0x777777 })
 var cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
@@ -20,4 +23,9 @@ function animate () {
 }
 animate()
 
-camera.position.z = 5
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize () {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+}
