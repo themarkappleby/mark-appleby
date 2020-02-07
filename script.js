bca2688d7789119ac6fa4d28ab692fa7769f2383
themarkappleby <themarkappleby@gmail.com>
2020-02-07 11:46:34 -0500

    init THREE.js and custom cursor

---
+++ script.js
@@ -0,0 +1,23 @@
+/* global window THREE */
+
+var scene = new THREE.Scene()
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+var renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+var geometry = new THREE.BoxGeometry()
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+var cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+function animate () {
+  window.requestAnimationFrame(animate)
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+  renderer.render(scene, camera)
+}
+animate()
+
+camera.position.z = 5
