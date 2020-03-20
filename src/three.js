3d7e1a61ee7d0b92b044469c1c3d823bbea92c1b
themarkappleby <themarkappleby@gmail.com>
2020-03-20 16:29:40 -0400

    skybox and minor polish

--- src/three.js
+++ src/three.js
@@ -16,23 +16,24 @@ export function initThree () {
   renderer.shadowMap.enabled = true
   renderer.setSize(canvas.offsetWidth, canvas.offsetHeight, false)
 
-  // ref: https://discourse.threejs.org/t/exporting-blender-scene-lighting-issues/11887/5
+  // Ref: https://discourse.threejs.org/t/exporting-blender-scene-lighting-issues/11887/5
   renderer.outputEncoding = THREE.sRGBEncoding
   renderer.physicallyCorrectLights = true
 
   getScene('scene.glb', function (gltf) {
     scene = gltf.scene
-    scene.fog = new THREE.FogExp2('#ffffff', 0.01)
 
     scene.traverse(node => {
       node.frustumCulled = false
     })
 
+    // Add skybox
+    scene.add(getSkybox())
+
     // Create camera
-    camera = new THREE.PerspectiveCamera(50, canvas.offsetWidth / canvas.offsetHeight, 0.01, 1000)
-    camera.position.set(5, 2, 23)
-    // camera.target = new THREE.Vector3(0, 0, 30)
-    camera.zoom = 1.5
+    camera = new THREE.PerspectiveCamera(50, canvas.offsetWidth / canvas.offsetHeight, 0.01, 30000)
+    camera.position.set(0, 2, 18)
+    camera.zoom = 2
     camera.updateProjectionMatrix()
     scene.add(camera)
 
@@ -40,11 +41,7 @@ export function initThree () {
     controls = new OrbitControls(camera, renderer.domElement)
     controls.enableDamping = true
     controls.dampingFactor = 0.1
-    /*
-    controls.minDistance = 1
-    controls.maxDistance = 15
-    */
-    controls.target.set(0, 2, 0)
+    controls.target.set(0, 3, 0)
 
     playAnimations(gltf)
 
@@ -52,17 +49,41 @@ export function initThree () {
   })
 }
 
+function getSkybox () {
+  const materialArray = []
+  const textureFt = new THREE.TextureLoader().load('assets/skybox_ft.jpg')
+  const textureBk = new THREE.TextureLoader().load('assets/skybox_bk.jpg')
+  const textureUp = new THREE.TextureLoader().load('assets/skybox_up.jpg')
+  const textureDn = new THREE.TextureLoader().load('assets/skybox_dn.jpg')
+  const textureRt = new THREE.TextureLoader().load('assets/skybox_rt.jpg')
+  const textureLf = new THREE.TextureLoader().load('assets/skybox_lf.jpg')
+
+  materialArray.push(new THREE.MeshBasicMaterial({ map: textureFt }))
+  materialArray.push(new THREE.MeshBasicMaterial({ map: textureBk }))
+  materialArray.push(new THREE.MeshBasicMaterial({ map: textureUp }))
+  materialArray.push(new THREE.MeshBasicMaterial({ map: textureDn }))
+  materialArray.push(new THREE.MeshBasicMaterial({ map: textureRt }))
+  materialArray.push(new THREE.MeshBasicMaterial({ map: textureLf }))
+
+  materialArray.forEach(material => {
+    material.side = THREE.BackSide
+  })
+
+  const skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000)
+  const skybox = new THREE.Mesh(skyboxGeo, materialArray)
+  return skybox
+}
+
 function playAnimations (mesh) {
   // Create an AnimationMixer, and get the list of AnimationClip instances
   mixer = new THREE.AnimationMixer(mesh.scene)
   var clips = mesh.animations
-  console.log('clips', clips)
 
   // Play a specific animation
   var clip = THREE.AnimationClip.findByName(clips, 'Chest Fall')
   var action = mixer.clipAction(clip)
-  console.log('action', action)
-  action.play()
+  action.clampWhenFinished = true
+  action.setLoop(THREE.LoopOnce).play()
 }
 
 // Import Blender models/textures/animations
