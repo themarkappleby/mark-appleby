a06be8936a27b3994b7d74693ec30fcc9e335e90
themarkappleby <themarkappleby@gmail.com>
2020-05-06 15:45:19 -0400

    improved logo emitter

--- src/logoEmitter.js
+++ src/logoEmitter.js
@@ -3,13 +3,23 @@ import rand from './utils/rand'
 let logoPool = []
 const activeLogos = []
 let running = false
+const mouse = {}
 
 function initLogoEmitter () {
+  document.onmousemove = e => {
+    mouse.x = e.clientX
+    mouse.y = e.clientY
+  }
+
   const logoContainer = document.querySelector('[data-src="logos"]')
   logoPool = Array.from(logoContainer.children)
 
   const trigger = document.querySelector('[data-action="emit-logos"]')
   trigger.onclick = e => {
+    e.target.classList.add('pulse')
+    window.setTimeout(() => {
+      e.target.classList.remove('pulse')
+    }, 200)
     e.preventDefault()
     if (!running) {
       running = true
@@ -37,8 +47,8 @@ function emit (logo) {
   activeLogos.push({
     name: logo.alt,
     el: logo,
-    x: 0,
-    y: 0,
+    x: mouse.x,
+    y: mouse.y,
     rotation: 0,
     life: 0,
     death: 100,
@@ -48,7 +58,6 @@ function emit (logo) {
       rotation: 1
     }
   })
-  // step()
 }
 
 function step () {
@@ -64,7 +73,9 @@ function step () {
       logo.y += logo.vector.y
       logo.rotation += logo.vector.rotation
 
-      logo.el.style.transform = `translate3d(${logo.x}px, ${logo.y}px, 0) scale(${1 - ((logo.life / 4) / logo.death)}) rotate(${logo.rotation}deg)`
+      const scale = 1 - ((logo.life / 4) / logo.death)
+
+      logo.el.style.transform = `translate3d(${logo.x}px, ${logo.y}px, 0) scale3d(${scale}, ${scale}, ${scale}) rotate3d(0, 0, 1, ${logo.rotation}deg)`
     }
   })
   if (!activeLogos.length) {
