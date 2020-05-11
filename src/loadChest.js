c28e7fa20654d9eb99aab76425517a2f7a6f1799
themarkappleby <themarkappleby@gmail.com>
2020-05-11 16:15:10 -0400

    functional chest pickHelper hitbox

--- src/loadChest.js
+++ src/loadChest.js
@@ -3,38 +3,62 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 // import { TweenMax, TimelineMax, Power1, Back } from 'gsap'
+import { TweenMax, Power1 } from 'gsap'
 // import rand from './utils/rand'
 
 const FRAME_RATE = 20 / 1000
+const helpers = []
 
 function loadChest ({ path, canvas }) {
   const renderer = initRenderer(canvas)
   const camera = initCamera(canvas)
   loadGLTF(path, gltf => {
-    // console.log('gltf', gltf)
     const scene = new THREE.Scene()
     scene.children = gltf.scene.children
-    // const scene = gltf.scene
-    // console.log('Imported Scene', scene)
-    // console.log('Default Scene', basicScene)
     scene.childrenMap = {}
     scene.traverse(node => {
       scene.childrenMap[node.name] = node
       if (node.material && node.material.roughness) {
         node.material.roughness = 0
       }
+
+      /*
+      var box = new THREE.BoxHelper(node, 0xff0000)
+      node.updateMatrixWorld(true)
+      box.applyMatrix4(node.matrixWorld)
+      helpers.push(box)
+      scene.add(box)
+      */
+
       node.frustumCulled = false // TODO: something weird with chest geometry, needs investigation
       node.castShadow = true
     })
     scene.add(initFloor())
     scene.add(initLight())
+    const pickHelper = initPickHelper()
+    scene.add(pickHelper)
+    scene.childrenMap.Pick_Helper = pickHelper
+    console.log(scene.childrenMap)
 
     const mixer = getAnimationMixer(scene, gltf.animations)
     render(renderer, scene, camera, mixer)
-    // initMouseTracking(renderer, scene, camera)
+    initMouseTracking(renderer, scene, camera)
     initResizeTracking(renderer, canvas, camera)
     // animate(scene)
   })
+}
+
+// Three JS Raycasting does not work with skinned meshes (of which the Chest is one), as noted here: https://stackoverflow.com/questions/55462615/three-js-raycast-on-skinning-mesh. Since my geometry is simply a GPU picking library like: https://github.com/bzztbomb/three_js_gpu_picking seems like overkills. Instead I'll have a simple transparent cube follow the mesh and act as the pick target.
+function initPickHelper () {
+  const geometry = new THREE.BoxGeometry(1.8, 1.5, 2)
+  const material = new THREE.MeshBasicMaterial({
+    color: 0x44aa88,
+    opacity: 0,
+    transparent: true
+  })
+  const pickHelper = new THREE.Mesh(geometry, material)
+  pickHelper.position.set(0.1, 0.8, 1)
+  return pickHelper
 }
 
 function getAnimationMixer (scene, clips) {
@@ -54,7 +78,6 @@ function initResizeTracking (renderer, canvas, camera) {
   }
 }
 
-/*
 function chestUp (scene) {
   TweenMax.to(scene.childrenMap.Chest_Bone.position, {
     y: -0.6,
@@ -110,7 +133,6 @@ function chestDown (scene) {
     ease: Power1.easeOut
   })
 }
-*/
 
 /*
 function animate (scene) {
@@ -155,6 +177,7 @@ function initRenderer (canvas) {
 function initCamera (canvas) {
   const camera = new THREE.PerspectiveCamera(50, canvas.offsetWidth / canvas.offsetHeight, 0.01, 30000)
   camera.position.set(0, 0.5, 6.5)
+  // camera.position.set(0, 0.5, 5)
   camera.fov = 55
   camera.updateProjectionMatrix()
   return camera
@@ -181,7 +204,8 @@ function initLight () {
   const shadowMapSize = 2
   // const light = new THREE.DirectionalLight(0xffffff, 0.4)
   const light = new THREE.DirectionalLight(0xffffff, 0.4)
-  light.position.set(0, 5, 0)
+  light.position.set(1, 5, 5)
+  // light.position.set(0, 5, 0)
   // light.position.set(3, 3, 3)
   light.castShadow = true
   light.shadow.radius = 3
@@ -195,37 +219,38 @@ function initLight () {
 function render (renderer, scene, camera, mixer) {
   mixer.update(FRAME_RATE)
   // console.log(mixer)
+  helpers.forEach(helper => {
+    helper.update()
+  })
   renderer.render(scene, camera)
   requestAnimationFrame(() => {
     render(renderer, scene, camera, mixer)
   })
 }
 
-/*
 function initMouseTracking (renderer, scene, camera) {
   const canvas = renderer.domElement
   const raycaster = new THREE.Raycaster()
+  const pickHelper = scene.childrenMap.Pick_Helper
   window.onmousemove = event => {
     const pos = getPickPosition(event, canvas)
 
-    const chest = scene.childrenMap.Chest_Empty
-    chest.lookAt(new THREE.Vector3(pos.x, pos.y, 15))
-
-    const island = scene.childrenMap.Island
-    island.lookAt(new THREE.Vector3(pos.x, pos.y, 30))
+    scene.childrenMap.Scene_Empty.lookAt(new THREE.Vector3(pos.x, pos.y, 15))
+    // scene.childrenMap.Scene_Empty.lookAt(new THREE.Vector3(pos.x, pos.y, 2))
 
     raycaster.setFromCamera(new THREE.Vector2(pos.x, pos.y), camera)
-    var intersects = raycaster.intersectObjects(chest.children, true)
+    var intersects = raycaster.intersectObjects([pickHelper])
+
+    // var intersects = raycaster.intersectObjects(scene.childrenMap.Chest_Empty.children, true)
     if (intersects.length) {
+      console.log(intersects)
       chestUp(scene)
     } else {
       chestDown(scene)
     }
   }
 }
-*/
 
-/*
 function getPickPosition (event, canvas) {
   const pos = getCanvasRelativePosition(event, canvas)
   return {
@@ -233,9 +258,7 @@ function getPickPosition (event, canvas) {
     y: (pos.y / canvas.height) * -2 + 1
   }
 }
-*/
 
-/*
 function getCanvasRelativePosition (event, canvas) {
   const rect = canvas.getBoundingClientRect()
   return {
@@ -243,6 +266,5 @@ function getCanvasRelativePosition (event, canvas) {
     y: (event.clientY - rect.top) * canvas.height / rect.height
   }
 }
-*/
 
 export default loadChest
