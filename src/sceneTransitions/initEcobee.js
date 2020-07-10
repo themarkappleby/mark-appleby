dd1115ecc8e254e7bb40c6745bb7f6394bab1dfc
themarkappleby <themarkappleby@gmail.com>
2020-07-10 15:10:42 -0400

    further mobile responsive improvements

--- src/sceneTransitions/initEcobee.js
+++ src/sceneTransitions/initEcobee.js
@@ -2,6 +2,8 @@ import gsap from 'gsap'
 import { show } from '../utils/showHide'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
+const SIDE_BY_SIDE_BP = 1000
+
 function ecobee (chest) {
   return () => {
     chest.gotoAndPlay('ecobee')
@@ -15,18 +17,37 @@ function ecobee (chest) {
       opacity: 0,
       duration: 1
     })
-    tl.to('.home, .home-overlay', {
-      left: '-25%',
-      opacity: 0,
-      duration: 2,
-      ease: 'power1.easeOut'
-    }, 0)
-    tl.to('.ecobee .hero-chest', {
-      left: '-25%',
-      marginLeft: 0,
-      duration: 2,
-      ease: 'power1.easeOut'
-    }, 0)
+    if (window.outerWidth >= SIDE_BY_SIDE_BP) {
+      tl.to('.home, .home-overlay', {
+        left: '-25%',
+        opacity: 0,
+        duration: 2,
+        ease: 'power1.easeOut'
+      }, 0)
+      tl.to('.ecobee .hero-chest', {
+        left: '-25%',
+        marginLeft: 0,
+        duration: 2,
+        ease: 'power1.easeOut'
+      }, 0)
+    } else {
+      tl.to('.home-wrapper', {
+        bottom: '-25%',
+        duration: 2,
+        ease: 'power1.easeOut'
+      }, 0)
+      tl.to('.home-overlay', {
+        opacity: 0,
+        duration: 2
+      }, 0)
+      /*
+      tl.to('.ecobee .hero-chest', {
+        yPercent: 25,
+        duration: 2,
+        ease: 'power1.easeOut'
+      }, 0)
+      */
+    }
     tl.to('.ecobee .hero-white', {
       opacity: 0,
       duration: 2,
