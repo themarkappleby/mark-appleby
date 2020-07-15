be05c3ea5190f9539ce99641697caeade65614b6
themarkappleby <themarkappleby@gmail.com>
2020-07-15 18:09:42 -0400

    additional mobile formatting improvements

--- src/scrollEffects/initCardIntros.js
+++ src/scrollEffects/initCardIntros.js
@@ -1,19 +1,21 @@
 import gsap from 'gsap'
 
 function cardIntros (section) {
-  gsap.utils.toArray(`.${section} .card`).forEach(card => {
-    gsap.from(card, {
-      scrollTrigger: {
-        trigger: card,
-        toggleActions: 'restart reset restart reset'
-      },
-      y: 50,
-      scale: 1.1,
-      opacity: 0,
-      duration: 1.5,
-      ease: 'power1'
+  if (window.width > 960) {
+    gsap.utils.toArray(`.${section} .card`).forEach(card => {
+      gsap.from(card, {
+        scrollTrigger: {
+          trigger: card,
+          toggleActions: 'restart reset restart reset'
+        },
+        y: 50,
+        scale: 1.1,
+        opacity: 0,
+        duration: 1.5,
+        ease: 'power1'
+      })
     })
-  })
+  }
 }
 
 export default cardIntros
--- src/scrollEffects/initParallax.js
+++ src/scrollEffects/initParallax.js
@@ -1,18 +1,20 @@
 import gsap from 'gsap'
 
 function init (section) {
-  gsap.utils.toArray(`.${section} .hero`).forEach(hero => {
-    const chest = hero.querySelector('.hero-chest')
-    gsap.to(chest, {
-      yPercent: -70,
-      scrollTrigger: {
-        trigger: hero,
-        scrub: 0.15,
-        start: 'top top',
-        end: 'bottom top'
-      }
+  if (window.width > 960) {
+    gsap.utils.toArray(`.${section} .hero`).forEach(hero => {
+      const chest = hero.querySelector('.hero-chest')
+      gsap.to(chest, {
+        yPercent: -70,
+        scrollTrigger: {
+          trigger: hero,
+          scrub: 0.15,
+          start: 'top top',
+          end: 'bottom top'
+        }
+      })
     })
-  })
+  }
 }
 
 export default init
