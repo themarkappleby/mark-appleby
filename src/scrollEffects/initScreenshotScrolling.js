9e737dae71512a83e162e333c3834a017ee95124
themarkappleby <themarkappleby@gmail.com>
2020-07-16 15:40:13 -0400

    mobile updates

--- src/scrollEffects/initScreenshotScrolling.js
+++ src/scrollEffects/initScreenshotScrolling.js
@@ -1,29 +1,33 @@
 import gsap from 'gsap'
 
 function screenshotScrolling (section) {
-  console.log(1)
-  if (window.innerWidth > 960) {
-    console.log(2)
-    gsap.utils.toArray(`.${section} .project-previewFrame`).forEach(frame => {
-      console.log(3)
-      const image = frame.children[0]
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: frame,
-          scrub: 0.25,
-          start: 'top bottom',
-          end: 'bottom top'
-        }
-      })
+  gsap.utils.toArray(`.${section} .project-previewFrame`).forEach(frame => {
+    const image = frame.children[0]
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: frame,
+        scrub: 0.25,
+        start: 'top bottom',
+        end: 'bottom top'
+      }
+    })
+    if (window.innerWidth > 960) {
       tl.to(frame, {
         rotationY: -20,
         y: -125
       }, 0)
-      tl.to(image, {
-        yPercent: -25,
-        delay: 0.2
+    } else {
+      tl.to(frame, {
+        rotationY: -20
       }, 0)
-    })
+    }
+    tl.to(image, {
+      yPercent: -25,
+      delay: 0.2
+    }, 0)
+  })
+
+  if (window.innerWidth > 960) {
     gsap.utils.toArray(`.${section} .project-previewBackground`).forEach(bkg => {
       gsap.to(bkg, {
         scrollTrigger: {
