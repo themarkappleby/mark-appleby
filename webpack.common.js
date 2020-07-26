c012a2fd24df8727f9b280038f71f5c47cb87c4a
themarkappleby <themarkappleby@gmail.com>
2020-07-26 16:42:21 -0400

    put three.js modules through babel

--- webpack.common.js
+++ webpack.common.js
@@ -21,7 +21,7 @@ module.exports = {
     rules: [
       {
         test: /\.m?js$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules\/(?!(three)\/).*/,
         use: {
           loader: 'babel-loader',
           options: {
