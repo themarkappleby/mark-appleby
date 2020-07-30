94611c86f9af6db4887d480c5ee40a6879a3a017
themarkappleby <themarkappleby@gmail.com>
2020-07-30 10:36:45 -0400

    improved Firefox performance

--- src/scrollEffects/index.js
+++ src/scrollEffects/index.js
@@ -12,24 +12,16 @@ function init (section) {
   initAppleby(section)
   initSwapChests(section)
   initBadges(section)
-  if (!isFirefox()) {
-    // TODO: For some reason Firefox performance with these animations is terrible. Needs further investigation but disabling them for now.
-    initScreenshotScrolling(section)
-    initCardIntros(section)
-    initParallax(section)
-    initScrollIndicators(section)
-  }
+  initScreenshotScrolling(section)
+  initCardIntros(section)
+  initParallax(section)
+  initScrollIndicators(section)
 }
 
 function flush () {
   ScrollTrigger.getAll().forEach(trigger => {
     trigger.kill()
   })
-}
-
-function isFirefox () {
-  var browser = navigator.userAgent.toLowerCase()
-  return browser.indexOf('firefox') > -1
 }
 
 export default init
