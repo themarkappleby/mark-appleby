5ddff029d81cd6b32c1bf69dfdc1f6eb6b9b73f2
themarkappleby <themarkappleby@gmail.com>
2020-08-17 20:33:36 -0400

    added scroll effect to project intros

---
+++ src/scrollEffects/initProjectIntro.js
@@ -0,0 +1,18 @@
+import gsap from 'gsap'
+
+function initProjectIntro (section) {
+  gsap.from(`.${section} .project-intro *`, {
+    y: 20,
+    opacity: 0,
+    duration: 0.8,
+    stagger: 0.2,
+    ease: 'power1.out',
+    scrollTrigger: {
+      trigger: `.${section} .project-intro`,
+      start: 'top 60%',
+      once: true
+    }
+  })
+}
+
+export default initProjectIntro
