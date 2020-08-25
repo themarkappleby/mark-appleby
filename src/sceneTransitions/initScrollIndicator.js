d1bfad8e174d4268a14db07417ea379c359e0946
themarkappleby <themarkappleby@gmail.com>
2020-08-25 09:42:23 -0400

    variable scroll indicator autokill based on breakpoint

--- src/sceneTransitions/initScrollIndicator.js
+++ src/sceneTransitions/initScrollIndicator.js
@@ -1,15 +1,17 @@
 import gsap from 'gsap'
 
+const SIDE_BY_SIDE_BP = 1000
+
 function initScrollIndicator (item) {
   const hero = item.closest('.hero')
   item.style.zIndex = 5
   item.addEventListener('click', e => {
     gsap.to(window, {
-      duration: 2.5,
+      duration: window.outerWidth >= SIDE_BY_SIDE_BP ? 2.5 : 1.25,
       ease: 'power4.out',
       scrollTo: {
         y: hero.clientHeight - 180,
-        autoKill: true
+        autoKill: window.outerWidth >= SIDE_BY_SIDE_BP
       }
     })
   })
